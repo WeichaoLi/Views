b914467c17c597d131da11bba6f974f62d763691
WeichaoLi <liweichao@liuxiangharus-Mac-mini.local>
2014-09-11 13:03:11 +0800

    Initial Commit

---
+++ Views/LWCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LWCAppDelegate.h
+//  Views
+//
+//  Created by 李伟超 on 14-9-11.
+//  Copyright (c) 2014年 LWC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LWCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
